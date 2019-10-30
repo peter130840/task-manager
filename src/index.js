605b6be697b00a49c420fa837b2e95a021b5b6ce
Peter <peter130840@hotmail.com>
2019-10-30 20:50:56 +0800

    Add endpoint /users/login

--- src/index.js
+++ src/index.js
@@ -15,18 +15,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const bcrypt = require('bcryptjs')
-
-const myFunction = async () => {
-    const password = 'Red12345'
-    const hashPassword = await bcrypt.hash(password, 8)
-
-    console.log(password)
-    console.log(hashPassword)
-
-    const isMatch = await bcrypt.compare('Red12345!', hashPassword)
-    console.log(isMatch)
-}
-
-myFunction()
